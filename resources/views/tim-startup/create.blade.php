61d5b86ec91c3982581727bd05292014ef42e93b
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2021-07-27 16:30:32 +0700

    test remote branch

--- resources/views/tim-startup/create.blade.php
+++ resources/views/tim-startup/create.blade.php
@@ -10,7 +10,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Tambah Tim Startup</h3>
+                    <h3>Tambah Tim Startup Test</h3>
                 </div>
             </div>
         </div>
